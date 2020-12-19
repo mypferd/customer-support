c38bf3313a8a703cf904b6c498bf57c6a6292cb8
my <maye_public@163.com>
2020-12-20 00:23:51 +0800

    some codes commit 04

--- cutomer-support-v1/src/main/java/com/my/site/entities/UserPrincipal.java
+++ cutomer-support-v1/src/main/java/com/my/site/entities/UserPrincipal.java
@@ -1,14 +1,42 @@
 package com.my.site.entities;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UserPrincipal_Username", columnNames = "Username")
+})
+@XmlAccessorType(XmlAccessType.NONE)
+@JsonAutoDetect(creatorVisibility = JsonAutoDetect.Visibility.NONE,
+          fieldVisibility = JsonAutoDetect.Visibility.NONE,
+          getterVisibility = JsonAutoDetect.Visibility.NONE,
+          isGetterVisibility = JsonAutoDetect.Visibility.NONE,
+          setterVisibility = JsonAutoDetect.Visibility.NONE)
 public class UserPrincipal implements UserDetails, CredentialsContainer, Cloneable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String username;
+    private byte[] hashedPassword;
+    private Set<UserAuthority> authorities = new HashSet<>();
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
     @Override
     public void eraseCredentials() {
 
