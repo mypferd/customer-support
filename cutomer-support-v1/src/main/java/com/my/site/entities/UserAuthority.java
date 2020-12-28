acea36e646ad3c0f9c908374f6fc2f6c112024e0
my <maye_public@163.com>
2020-12-28 23:48:50 +0800

    some codes commit 05

--- cutomer-support-v1/src/main/java/com/my/site/entities/UserAuthority.java
+++ cutomer-support-v1/src/main/java/com/my/site/entities/UserAuthority.java
@@ -1,4 +1,23 @@
 package com.my.site.entities;
 
-public class UserAuthority {
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserAuthority implements GrantedAuthority {
+
+    private String authority;
+
+    public UserAuthority() {}
+
+    public UserAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
--- cutomer-support-v1/src/main/java/com/my/site/entities/UserPrincipal.java
+++ cutomer-support-v1/src/main/java/com/my/site/entities/UserPrincipal.java
@@ -1,17 +1,18 @@
 package com.my.site.entities;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.security.core.CredentialsContainer;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import java.util.Collection;
+import javax.xml.bind.annotation.XmlElement;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -37,43 +38,129 @@ public class UserPrincipal implements UserDetails, CredentialsContainer, Cloneab
     private boolean credentialsNonExpired;
     private boolean enabled;
 
+    @Id
+    @Column(name = "UserId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @XmlElement
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "HashedPassword")
+    public byte[] getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(byte[] hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public void setAuthorities(Set<UserAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
     @Override
     public void eraseCredentials() {
-
+        this.hashedPassword = null;
     }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "UserPrincipal_Authority", joinColumns = {
+            @JoinColumn(name = "UserId", referencedColumnName = "UserId")
+    })
+    public Set<UserAuthority> getAuthorities() {
+        return this.authorities;
     }
 
     @Override
+    @Transient
     public String getPassword() {
-        return null;
+        return this.getHashedPassword() == null ? null :
+                new String(this.getHashedPassword(), StandardCharsets.UTF_8);
     }
 
     @Override
+    @XmlElement
+    @JsonProperty
     public String getUsername() {
-        return null;
+        return this.username;
     }
 
     @Override
+    @XmlElement
+    @JsonProperty
     public boolean isAccountNonExpired() {
-        return false;
+        return this.accountNonExpired;
     }
 
     @Override
+    @XmlElement
+    @JsonProperty
     public boolean isAccountNonLocked() {
-        return false;
+        return this.accountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return this.credentialsNonExpired;
     }
 
     @Override
+    @XmlElement
+    @JsonProperty
     public boolean isEnabled() {
-        return false;
+        return this.enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof UserPrincipal &&
+                ((UserPrincipal)o).id == this.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.username.hashCode();
+    }
+
+    @Override
+    protected UserPrincipal clone() {
+        try {
+            return (UserPrincipal)super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.username;
     }
 }
